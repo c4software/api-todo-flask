c1eaa0a0466e99b7151e7e64743c158443e51e07
Valentin Brosseau <c4software@gmail.com>
2018-01-30 21:49:23 +0100

    Ajout method

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/api/list")
-@app.route("/api/")
+@app.route("/api/todo")
 def list():
+    return ""
+
+@app.route("/api/todo", methods=['POST'])
+def save():
+    return ""
+
+@app.route("/api/todo/done/<id>")
+def done(id):
+    return ""
+
+@app.route("/api/todo/delete/<id>", methods=['DELETE'])
+def delete(id):
     return ""
 
 if __name__ == '__main__':
