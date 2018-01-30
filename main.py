d266387d694a34c766e87a3b43829427d42f692d
Valentin Brosseau <c4software@gmail.com>
2018-01-30 22:15:23 +0100

    Ajout delete and docstring

--- main.py
+++ main.py
@@ -5,26 +5,34 @@ todo_memory = []
 
 @app.route("/api/todo")
 def getList():
+    """ Get the current state of the todo « memory » list """
     return jsonify(todo_memory)
 
 @app.route("/api/todo", methods=['POST'])
 def save():
     return ""
 
-@app.route("/api/todo/done/<id>")
-def done(id):
+@app.route("/api/todo/done/<current_id>")
+def done(current_id):
+    """ Mark a todo as done """
     # Id in todo ?
-    if id in todo_memory:
-        current = todo_memory[id]
+    if current_id in todo_memory:
+        current = todo_memory[current_id]
         current["termine"] = True # Mark As done
-        todo_memory[id] = current # and Save
+        todo_memory[current_id] = current # and Save
         return jsonify({"success": True})
     else:
         return jsonify({"success": False})
 
-@app.route("/api/todo/delete/<id>", methods=['DELETE'])
-def delete(id):
-    return ""
+@app.route("/api/todo/delete/<current_id>", methods=['DELETE'])
+def delete(current_id):
+    """ Remove element from the todo_memory """
+    # current_id exist and mark as done ?
+    if current_id in todo_memory and todo_memory[current_id]["termine"]:
+        del todo_memory[current_id] # Remove the data
+        return jsonify({"success": True})
+    else:
+        return jsonify({"success": False})
 
 if __name__ == '__main__':
     app.run()
